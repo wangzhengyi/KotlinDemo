d33be8cb022436e5dc780a608c892d150e542c4a
wangzhengyi.wzy <wangzhengyi.wzy@bytedance.com>
2019-09-18 11:00:55 +0800

    配置Kotlin环境

---
+++ app/src/main/java/com/ss/android/auto/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.ss.android.auto
+
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        message.text = "hello kotlin"
+    }
+}
